7e54db32141e27bcf9eeec4608845b7ce11eac10
Dave Panofsky <dpanofsky@singleplatform.com>
2018-06-26 14:58:25 -0400

    add required configuration

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 xcode-select --install
-pip install files/pip-10.0.1-py2.py3-none-any.whl
-pip install ansible --user
+pip install -f wheels pip virtualenv
+virtualenv /tmp/mac-dev-playbook
+. /tmp/mac-dev-playbook/bin/activate
+pip install ansible
 ansible-galaxy install -r requirements.yml
 
 vi config.yml
