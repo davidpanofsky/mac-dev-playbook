7e3fbf17d8fef7dfc1b1415ae5bc02f42aa69076
Dave Panofsky <dpanofsky@singleplatform.com>
2018-06-27 11:34:30 -0400

    messaging in install script

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ virtualenv /tmp/mac-dev-playbook
 pip install ansible
 
 cp -n files/mac-dev-playbook ~/.config
+
+echo 'Default configuration file will be opened in an editor. At a minimum, set your name and email address.'
+read -p 'Press Return to continue'
 vi ~/.config/mac-dev-playbook
 ./provision.sh
