6f98162162fea8a5fe5f70a499ac4ff77e58056a
Dave Panofsky <dpanofsky@singleplatform.com>
2018-06-27 12:01:01 -0400

    add virtualenv

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-pip install -f wheels pip virtualenv
-virtualenv /tmp/mac-dev-playbook
-. /tmp/mac-dev-playbook/bin/activate
-pip install ansible
+if [ ! -f ~/.config/mac-dev-playbook ]; then
+  cp files/mac-dev-playbook ~/.config
 
-cp -n files/mac-dev-playbook ~/.config
-
-echo 'Default configuration file will be opened in an editor. At a minimum, set your name and email address.'
-read -p 'Press Return to continue'
-vi ~/.config/mac-dev-playbook
-./provision.sh
+  echo 'Default configuration file will be opened in an editor. At a minimum, set your name and email address.'
+  read -p 'Press Return to continue'
+  vi ~/.config/mac-dev-playbook
+fi
+./env/bin/ansible-playbook main.yml -K -i inventory
--- provision.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -K -i inventory
